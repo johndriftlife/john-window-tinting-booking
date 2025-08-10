d74b28124d172fb327c3f0edcc23e1e459753b38
johndriftlife <nathanjohnsxm@gmail.com>
2025-08-09 20:02:38 -0400

    Add files via upload

---
+++ frontend/pages/success.js
@@ -0,0 +1,11 @@
+export default function Success(){
+  return (
+    <div className="container">
+      <h1 className="center">Thank you!</h1>
+      <p className="center">We received your deposit. You’ll also get an email/SMS confirmation.</p>
+      <div className="center" style={{marginTop:12}}>
+        <a className="link" href="/">Back to home</a>
+      </div>
+    </div>
+  )
+}
